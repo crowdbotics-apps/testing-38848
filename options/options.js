18716d7c7e89f71812e148dcf2d082aac06e32aa
langton muchemwa <muchemwal@gmail.com>
2023-02-10 20:03:00 +0200

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "testing-38848",
   url: "https://testing-38848.botics.co",
   api: "https://testing-38848.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
